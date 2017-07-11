0dd143816477adf7d3562db206461deb1ee7bf16
ia <isaac.ardis@gmail.com>
2017-07-11 12:54:36 -0500

    fix deploy script typo

--- deploy.sh
+++ deploy.sh
@@ -4,4 +4,4 @@
 cp SENDME.txt SENDME-$(janus version -format v%M.%m.%P+%C-%S).dep.txt
 
 # Ship it squirrel.
-janus deploy -to isaac-tests/squirrel/$(janus version -format %M.%m.x)/ -files ./*.dep.txt -key ./isaac-test-gcp-service-key-enc.json
+janus deploy -to isaac-tests/squirrel/$(janus version -format %M.%m.x)/ -files ./*.dep.txt -key ./isaac-test-gcp-service-key.enc.json
