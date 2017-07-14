9c7e6359ecf5846d77a9bbc82bcff0f167a0c02f
ia <isaac.ardis@gmail.com>
2017-07-14 09:20:41 -0500

    mimick go-eth for trying out deploys

--- deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-# Prepare for liftoff.
-cp SENDME.txt SENDME-$(janus version -format v%M.%m.%P+%C-%S).dep.txt
-
-# Ship it squirrel.
-janus deploy -to isaac-tests/squirrel/$(janus version -format %M.%m.x)/ -files ./*.dep.txt -key ./isaac-test-gcp-service-key.enc.json
