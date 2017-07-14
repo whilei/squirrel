41fdf414584770f223ee5a5c62d7add192e21d19
ia <isaac.ardis@gmail.com>
2017-07-14 09:36:07 -0500

    problem: janus key requires argument
    solution: use quotes

--- deploy.sh
+++ deploy.sh
@@ -2,4 +2,4 @@
 
 mkdir ./deploy
 mv ./dist/*.tar.gz ./dist/*.zip ./deploy/
-janus deploy -to isaac-tests/go-squirrel/$(janus version -format %M.%m.x) -files ./deploy/* -key ./isaac-test-gcp-service-key.enc.json
+janus deploy -to="isaac-tests/go-squirrel/$(janus version -format %M.%m.x)" -files="./deploy/*" -key="./isaac-test-gcp-service-key.enc.json"
