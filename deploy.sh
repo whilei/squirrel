eaed87f2dfc10d0094ad36c1738422ec1035a72f
ia <isaac.ardis@gmail.com>
2017-07-14 09:27:25 -0500

    problem: deploy cannot be mutliple entries
    solutino: use script

---
+++ deploy.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+mkdir ./deploy
+mv ./dist/*.tar.gz ./dist/*.zip ./deploy/
+janus deploy -to isaac-tests/go-squirrel/$(janus version -format %M.%m.x) -files ./deploy/* -key ./isaac-test-gcp-service-key.enc.json
